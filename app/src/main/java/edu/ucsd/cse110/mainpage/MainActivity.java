d2f24b0270ab90ed762cf81e96b03007227a31ca
Gastly <originalmrcool@gmail.com>
2019-02-12 22:28:50 -0800

    logging in finally worked

--- app/src/main/java/edu/ucsd/cse110/mainpage/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/mainpage/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.ucsd.cse110.mainpage;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,9 +9,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import edu.ucsd.cse110.mainpage.fitness.FitnessService;
+import edu.ucsd.cse110.mainpage.fitness.FitnessServiceFactory;
+import edu.ucsd.cse110.mainpage.fitness.GoogleFitAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private TextView textSteps;
+
+    public static final String FITNESS_SERVICE_KEY = "FITNESS_SERVICE_KEY";
+    private String fitnessServiceKey = "GOOGLE_FIT";
+
+    private static final String TAG = "MainActivity";
+
+    private FitnessService fitnessService;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,12 +47,43 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences userSharedPref = getSharedPreferences("userdata", MODE_PRIVATE);
+        userSharedPref.edit().clear().commit();
+        int height = userSharedPref.getInt("height",0);
+        if (height == 0) {
+            Intent promptHeightIntent = new Intent(this, EnterHeightActivity.class);
+            startActivityForResult(promptHeightIntent, 0);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textSteps = findViewById(R.id.stepsView);
+
+        FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(MainActivity stepCountActivity) {
+                return new GoogleFitAdapter(stepCountActivity);
+            }
+        });
+
+        fitnessService = FitnessServiceFactory.create(fitnessServiceKey,this);
+
+        fitnessService.setup();
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            finish();
+        }
+
+    }
+
+    public void setStepCount(long stepCount) {
+        textSteps.setText(String.valueOf(stepCount));
+    }
 }
